316b72cb1ae3981686837553b49fb95008e51597
Eric Humphrey <ejhumphrey@spotify.com>
2018-08-19 15:33:21 -0400

    Added a mini-docstring to the VGGish api

--- openmic/vggish/__init__.py
+++ openmic/vggish/__init__.py
@@ -1,8 +1,25 @@
+'''
+VGGish API
+
+Provides a simple interface to the VGGish model:
+
+Inputs
+------
+ * waveform_to_examples: tf.Examples from an ndarray
+ * soundfile_to_examples: tf.Examples from a sound file
+
+Transforms
+----------
+ * transform: Times and VGGish features (ndarray) from tf.Examples
+ * postprocess: PCA'ed embeddings from VGGish features
+
+'''
 
 from .params import *
+
 from .inputs import waveform_to_examples, soundfile_to_examples
-from .postprocessor import Postprocessor
 from .model import transform
+from .postprocessor import Postprocessor
 
 __pproc__ = Postprocessor(PCA_PARAMS)
 postprocess = __pproc__.postprocess
